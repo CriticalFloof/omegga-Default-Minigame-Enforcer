c7f07375a45c134ce9a37ffc6e65586c57022f1a
CriticalFloof <104379154+CriticalFloof@users.noreply.github.com>
2022-12-13 21:52:29 -0500

    Add files via upload

---
+++ omegga.plugin.ts
@@ -0,0 +1,65 @@
+import OmeggaPlugin, { OL, PS, PC } from 'omegga';
+
+type Config = { foo: string };
+type Storage = { bar: string };
+
+export default class Plugin implements OmeggaPlugin<Config, Storage> {
+  omegga: OL;
+  config: any;
+  store: PS<Storage>;
+
+  constructor(omegga: OL, config: PC<Config>, store: PS<Storage>) {
+    this.omegga = omegga;
+    this.config = config;
+    this.store = store;
+  }
+
+  async init() {
+    //1. once per second, finds all players currently not in any minigame.
+    const authorizedRoles = this.config["authorized-roles"]
+
+    setInterval(async ()=>{
+      let players = Omegga.players
+      for (let i = 0; i < players.length; i++) {
+        const player = players[i];
+        //2. remove player and continue if they have appropriate role.
+        const roles = player.getRoles()
+        let isAuthorized = false
+        for (let j = 0; j < roles.length; j++) {
+          const role = roles[j];
+          for (let k = 0; k < authorizedRoles.length; k++) {
+            const authorizedRole = authorizedRoles[k];
+            if(role === authorizedRole){
+              isAuthorized = true
+            }
+          }
+        }
+        if(!isAuthorized){
+          
+          //3. check each player for being in a minigame if not, move them to the default minigame. 
+          //3.1 get all actors' teams. (2147482533 is the 'global minigame' team)
+          const reg = new RegExp(
+            /BP_PlayerState_C .+?PersistentLevel\.(?<state>BP_PlayerState_C_\d+)\.Team = .+PersistentLevel.+BP_Team_C_(?<team>\d+)/
+          );
+          const regExpMatchArray: RegExpMatchArray[] = await this.omegga.watchLogChunk( 'GetAll BP_PlayerState.BP_PlayerState_C Team', reg,
+          {
+            timeoutDelay: 500
+          })
+          for (let j = 0; j < regExpMatchArray.length; j++) {
+            const { team, state } = regExpMatchArray[j].groups;
+            if(player.state === state && team === "2147482533") {
+              //3.2 set everyone who doesn't match criteria to the preferred minigame index.
+              player.setMinigame(this.config['Default-minigame'])
+            }
+          }
+        }
+      }
+    }, this.config['check-interval']*1000)
+
+    return { registeredCommands: [] };
+  }
+
+  async stop() {
+    // Anything that needs to be cleaned up...
+  }
+}
